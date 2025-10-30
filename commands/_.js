6771358cb32daf415742e2d8b2ec1c693ea682c4
Sakthi Jena R <127412012+Code-X-Sakthi@users.noreply.github.com>
2025-10-30 20:17:25 +0530

    Add files via upload

---
+++ commands/_.js
@@ -0,0 +1,23 @@
+/*CMD
+  command: *
+  help: 
+  need_reply: false
+  auto_retry_time: 
+  folder: 
+  answer: 
+  keyboard: 
+  aliases: 
+  group: 
+CMD*/
+
+Bot.sendMessage(
+"*🎉🔰 Welcome In Our Premium Account Giveaway Bot*\n" +
+"----------------------------------------------🎉\n\n" +
+"*Join Our Giveaway Channel to use Our Bot*\n" +
+"» [Mad Tutorial](https://t.me/mad_tutor)\n" +
+"» [Mad Tutorial Store](https://t.me/madtutorialstore)\n" +
+"» [Free Premium Accounts](https://t.me/Mad_ttuts)\n" +
+"» [Mad Tutorial Logs](https://t.me/MadTutXLogs)\n" +
+"----------------------------------------------",
+{ parse_mode: "Markdown" }
+)
